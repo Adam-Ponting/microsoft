82729aba894eb7725b640bef3d0220959ef1378c
Adam Ponting <adam.ponting@gmail.com>
2020-10-09 17:13:56 +0100

    done 18

---
+++ beginners-series-to-javascript/my-first-project/math.js
@@ -0,0 +1,20 @@
+let num1 = 90;
+
+console.log('-- Basic Math --');
+console.log(num1 + 25);
+console.log(num1 - 25);
+console.log(num1 * 25);
+console.log(num1 / 25);
+
+console.log('-- Additional Math --');
+console.log(num1 % 1500);
+console.log(++num1);
+console.log(--num1);
+console.log(num1++);
+console.log(num1--);
+
+console.log('-- Math Object --');
+console.log(Math.PI);
+console.log(Math.sqrt(num1));
+
+console.log(0.01 + 0.02);
