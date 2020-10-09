e2af97da6b9f487a07e394d69ef088e4707f00aa
Adam Ponting <adam.ponting@gmail.com>
2020-10-09 16:44:46 +0100

    done 15

--- beginners-series-to-javascript/my-first-project/demo.js
+++ beginners-series-to-javascript/my-first-project/demo.js
@@ -1,6 +1,10 @@
 let str1 = 'JavaScript';
 let str2 = 'fun';
 
-console.log(`I am writing code in ${str1}`);
-console.log(`Formatting in ${str1} is fun`);
-console.log(`1 + 1 is ${Boolean(0111)}`);
+// console.log(`I am writing code in ${str1}`);
+// console.log(`Formatting in ${str1} is fun`);
+// console.log(`1 + 1 is ${Boolean(0111)}`);
+// console.log(new Number(1) instanceof Number);
+// console.log(typeof 1);
+console.log(0 == '');
+console.log(0 === '');
