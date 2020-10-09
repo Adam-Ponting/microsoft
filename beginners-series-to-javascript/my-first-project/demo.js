8c127ae0e7fb05b21d1765500d2aade8eaab0693
Adam Ponting <adam.ponting@gmail.com>
2020-10-09 16:09:55 +0100

    done 10

---
+++ beginners-series-to-javascript/my-first-project/demo.js
@@ -0,0 +1,14 @@
+var hello = 'Hello';
+console.log(hello);
+
+hello = 'Hello World';
+
+console.log(hello);
+if (true) {
+    let world = 'Hello World';
+    console.log(world);
+}
+
+const name = 'Adam';
+console.log(name);
+console.log(typeof name, name);
