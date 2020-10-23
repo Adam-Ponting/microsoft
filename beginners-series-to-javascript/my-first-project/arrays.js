8d8e760b6d3fc95d8722f0e3153b239fd63c4907
Adam Ponting <adam.ponting@gmail.com>
2020-10-23 09:33:36 +0100

    done 30

---
+++ beginners-series-to-javascript/my-first-project/arrays.js
@@ -0,0 +1,14 @@
+let arrayLength = 5;
+let arr1 = [];
+let arr2 = Array(arrayLength);
+let arr3 = new Array();
+let arr4 = new Array(arrayLength);
+
+console.log(arr1);
+console.log(arr2);
+console.log(arr3);
+console.log(arr4);
+console.log(arr1.length);
+console.log(arr2.length);
+console.log(arr3.length);
+console.log(arr4.length);
